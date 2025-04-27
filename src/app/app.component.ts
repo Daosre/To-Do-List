74169d3e0f6a840270b19c118131fb8736543808
Daorse <mmorpgplays5@gmail.com>
2025-04-27 21:47:32 +0200

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,22 @@
-import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
-export interface TodoItem {
-  id: number;
-  task: string;
-  completed: boolean;
-}
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  todolist: TodoItem[] = [];
-  newTask: string = '';
+  todo = '';
+  todolist: { id: number; name: string }[] = [];
 
-  addTask(): void {
-    if (this.newTask.trim() !== '') {
-      const newTodo: TodoItem = {
-        id: Date.now(),
-        task: this.newTask,
-        completed: false,
-      };
-      this.todolist.push(newTodo);
-      this.newTask = '';
-    }
+  addTask() {
+    this.todolist.push({ id: this.todolist.length + 1, name: this.todo });
   }
 
-  todoComplete(index: number): void {
-    this.todolist[index].completed = !this.todolist[index].completed;
-  }
-
-  deleteTask(id: number): void {
-    this.todolist = this.todolist.filter((element) => {
-      element.id !== id;
-    });
+  deleteTask(id: number) {
+    this.todolist = this.todolist.filter((element) => element.id != id);
   }
 }
